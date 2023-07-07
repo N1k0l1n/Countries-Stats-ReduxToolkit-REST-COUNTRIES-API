a569c3bda8601626afb9b6b5ee59282f74f948aa
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-07-07 15:32:30 +0200

    adding pagination

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,8 +1,11 @@
 import "./country.css";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+// Redux
 import { useSelector, useDispatch } from "react-redux";
 import { showAllCountries } from "../../app/features/countries/countriesAction";
-import Spinner from '../../assets/spinner/Spinner'
+// Components
+import Spinner from "../../assets/spinner/Spinner";
 
 const Country = () => {
   const { countriesData, loading, error, success } = useSelector(
@@ -10,6 +13,8 @@ const Country = () => {
   );
   const dispatch = useDispatch();
   const [countryData, setCountryData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const countriesPerPage = 8;
 
   useEffect(() => {
     dispatch(showAllCountries()).then(() => {
@@ -23,14 +28,24 @@ const Country = () => {
     });
   }, [dispatch, success, error]);
 
+  // Get current countries for pagination
+  const indexOfLastCountry = currentPage * countriesPerPage;
+  const indexOfFirstCountry = indexOfLastCountry - countriesPerPage;
+  const currentCountries = countryData.slice(
+    indexOfFirstCountry,
+    indexOfLastCountry
+  );
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <section className="country-container">
       {loading ? (
-        <Spinner/>
+        <Spinner />
       ) : (
-        countryData.map((item, index) => (
-          <div className="country-card" key={index}>
+        currentCountries.map((item, index) => (
+          <Link className="country-card" key={index}>
             <img
               src={item.flags.png}
               alt={item.flags.alt}
@@ -48,9 +63,30 @@ const Country = () => {
                 Capital: <span>{item.capital}</span>
               </p>
             </div>
-          </div>
+          </Link>
         ))
       )}
+
+      {/* Pagination */}
+      <div className="pagination-container">
+        <button
+          className={`pagination-item previous ${currentPage === 1 ? "disabled" : ""}`}
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <div className="page-indicator">
+          Page {currentPage} of {Math.ceil(countryData.length / countriesPerPage)}
+        </div>
+        <button
+          className={`pagination-item next ${currentPage === Math.ceil(countryData.length / countriesPerPage) ? "disabled" : ""}`}
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === Math.ceil(countryData.length / countriesPerPage)}
+        >
+          Next
+        </button>
+      </div>
     </section>
   );
 };
